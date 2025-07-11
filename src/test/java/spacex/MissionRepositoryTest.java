a5cbc01e66a5868cb7e902ce990741bdf69db2f4
kamil <kamil.krold@gmail.com>
2025-07-11 23:45:41 +0200

    Add implementation to fix tests

--- src/test/java/spacex/MissionRepositoryTest.java
+++ src/test/java/spacex/MissionRepositoryTest.java
@@ -2,16 +2,20 @@ package spacex;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import spacex.model.Mission;
+import spacex.model.MissionStatus;
+import spacex.repository.MissionRepository;
+import spacex.repository.MissionRepositoryImpl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MissionRepositoryTest {
 
-    private IMissionRepository missionRepo;
+    private MissionRepository missionRepo;
 
     @BeforeEach
     void setup() {
-        missionRepo = new MissionRepository();
+        missionRepo = new MissionRepositoryImpl();
     }
 
     @Test
@@ -25,7 +29,7 @@ class MissionRepositoryTest {
     void changeMissionStatus_updatesStatus() {
         missionRepo.addMission("Apollo");
         missionRepo.changeStatus("Apollo", MissionStatus.ENDED);
-        Mission mission = missionRepo.get("Apollo");
+        Mission mission = missionRepo.getMission("Apollo");
         assertEquals(MissionStatus.ENDED, mission.getStatus());
     }
 }
